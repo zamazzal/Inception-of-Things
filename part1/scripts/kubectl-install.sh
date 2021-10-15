42829926baa6ffae004906223de5efcb93bf23c2
zamazzal <zouhair.amazzal@gmail.com>
2021-10-15 19:03:20 +0100

    part1

---
+++ part1/scripts/kubectl-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+mkdir ~/.kube/
+cp -r /shared/kubeconfig ~/.kube/config
+cp -r ~/.kube /home/vagrant/
