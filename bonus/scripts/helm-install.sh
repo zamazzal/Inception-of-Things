6a62cf99b9a222003c483e8807145af24e4a63bf
Zouhair AMAZZAL <zouhair.amazzal@gmail.com>
2021-10-23 16:45:43 +0100

    bonus

---
+++ bonus/scripts/helm-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# setup helm
+wget https://get.helm.sh/helm-v3.6.3-linux-amd64.tar.gz
+tar -xvf helm-v3.6.3-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/helm
---
+++ bonus/scripts/setup-env.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# install docker
+apt-get update
+
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+apt-get install docker-ce docker-ce-cli containerd.io
+
+# install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+
+# install k3d
+wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+
+
+# create cluster
+k3d cluster create zamazzalCluster -p "8888:30080"
+
+# create namespaces
+kubectl create namespace argocd
+kubectl create namespace dev
+
+# setup argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+nohup kubectl port-forward svc/argocd-server -n argocd 8080:443 >> argocdlogs.log 2>&1 & 
+#user: admin
+echo "url : https://localhost:8080"
+echo "user: admin"
+echo -n "password: "
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+echo ""
